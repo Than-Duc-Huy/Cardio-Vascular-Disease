a04f195a8ff90749a62e845e160e528a013ef29b
Than Duc Huy <than_duc_huy@outlook.com>
2021-11-12 12:41:40 +0800

    2021-11-12

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
+# IMPORT LIBRARY
 import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import plotly.express as px
 
@@ -10,18 +12,21 @@ import plotly.express as px
 
 st.header("Cardiovascular disease risk diagnosis")
 
-@st.cache
+# LOAD CLEAN DATA
+@st.cache  # To store data in cache
 def loaddata():
-	data = pd.read_csv('databmi.csv')
+	data = pd.read_csv('data.csv')
 	return data
 
 
+num_attribs = ['age', 'height', 'weight', 'ap_hi', 'ap_lo']
 
 data = loaddata()
 
+# Split into 4 columns
 one, two, three, four = st.columns(4)
 gender = one.radio("Gender",('Male','Female'))
-gender = 2 if gender == 'Male'else 1
+gender = 1 if gender == 'Male'else 0
 
 one.write(gender)
 
@@ -29,9 +34,7 @@ age = two.slider("Age",5,100,20,1)
 height = three.slider("Height in cm",100,300,160,1)
 weight = four.slider("Weight in kg",20.0,150.0,50.0,0.1)
 
-st.session_state["weight"] = weight
-st.session_state["height"] = height
-
+# Split into 3 columns
 one2, two2, three2 = st.columns(3)
 smoke = one2.radio("Do you smoke?", ('Yes','No'))
 smoke = 1 if smoke == 'Yes' else 0
@@ -45,6 +48,7 @@ active = three2.radio("Do you exercise frequently?", ('Yes','No'))
 active = 1 if active == 'Yes' else 0
 three2.write(active)
 
+# Examinable Section
 st.write("#### Examinable")
 exam = st.radio("Do you have a recent health check?", ('Yes','No'), index = 1)
 if  exam == 'Yes':
@@ -69,7 +73,7 @@ else:
 	cholesterol = 1
 
 
-
+# Calculated Risk Section
 st.subheader("Calculated Risks")
 BMI = weight/(height/100)**2
 if BMI < 18.5:
@@ -84,6 +88,7 @@ st.write("According to your BMI of ",round(BMI,1), ", you are ", obesity)
 obesity = 0 if obesity == "Underweight" else (1 if obesity == "Healthy" else(2 if obesity =="Overweight" else 3))
 
 
+# Plot BMI Graph
 if "graph" not in st.session_state:
 	if "ax" not in st.session_state:
 		st.session_state.graph, st.session_state.ax = plt.subplots(1,figsize = (20,10))
@@ -97,52 +102,66 @@ if "graph" not in st.session_state:
 		st.session_state.graph.legend(prop = {'size': 20})
 
 
+# Highlight Red dot
 st.session_state.ax.scatter(x = weight, y = height, color = "red")
 with st.spinner("Plotting BMI Graph ..."):
 	st.pyplot(st.session_state.graph)
 st.session_state.ax.scatter(x = weight, y = height, color = "gray")
 
 
+# Construct the person's Variable
 person = pd.DataFrame({"age": [age],"gender": [gender],"height": [height],"weight": [weight],"ap_hi": [systolic],"ap_lo": [diastolic],"cholesterol": [cholesterol],"gluc": [glucose],"smoke": [smoke],"alco": [alcohol],"active": [active]})
 
 
 
-
+# Feature scaling
+scaler = StandardScaler()
+# Target feature
 data_Y = data["cardio"]
-
-# st.write("Your profile")
+# Model Score initialization
+score = 0
+# Model with Examinable data
 if exam == 'Yes':
-	data_X = data.drop(columns = ["cardio","BMI","Obesity"])
+	person[num_attribs] = scaler.fit_transform(person[num_attribs])
 	if 'model_exam' not in st.session_state:
+		data_X = data.drop(columns = ["cardio"])
+		data_X[num_attribs] = scaler.fit_transform(data_X[num_attribs])
 		with st.spinner("Initialising Random Forest Classifier with Examinable data"):
 			st.session_state.model_exam = RandomForestClassifier(max_depth = 9, n_estimators= 350, random_state = 42)
 			st.session_state.model_exam.fit(data_X, data_Y)
-	forest = st.session_state.model_exam
+			if 'model_exam_score' not in st.session_state:
+				st.session_state.model_exam_score = round(st.session_state.model_exam.score(data_X,data_Y),4)
 
+	forest = st.session_state.model_exam
+	score = st.session_state.model_exam_score
+
+
+# Model without Examinable data
 else :
 	exam_var = ["ap_hi","ap_lo","cholesterol","gluc"]
-	data = data.drop(columns = exam_var)
-	data_X = data.drop(columns = ["cardio","BMI","Obesity"])
 	person = person.drop(columns = exam_var)
+	person[['age','height','weight']] = scaler.fit_transform(person[['age','height','weight']])
 	if 'model' not in st.session_state:
+		data = data.drop(columns = exam_var)
+		data_X = data.drop(columns = ["cardio"])
+		data_X[['age','height','weight']] = scaler.fit_transform(data_X[['age','height','weight']])
 		with st.spinner("Initialising Random Forest Classifier without Examinable data"):
 			st.session_state.model = RandomForestClassifier(max_depth = 9, n_estimators= 350, random_state = 42)
 			st.session_state.model.fit(data_X, data_Y)
+			if 'model_score' not in st.session_state:
+				st.session_state.model_score = round(st.session_state.model.score(data_X,data_Y),4)
+
 	forest = st.session_state.model
+	score = st.session_state.model_score
 
 with st.spinner("Calculating risk..."):
 	cardio = forest.predict(person)
-	score = round(forest.score(data_X,data_Y),2)
-
 
 st.write("Using Random Forest, the with training accuracy ",score)
 if cardio == 0:
-	st.write("You are **NOT** at risk of having cardiovascular disease")
+	st.write("You are **NOT at risk** of having cardiovascular disease")
 else:
 	st.write("You are **AT RISK** of having cardiovascular disease")
 
 if exam == "No":
 	st.warning("Consider providing Examinable information for better prediction!")	
-
-
-
--- README.md
+++ README.md
@@ -1,28 +1,95 @@
-# EE0005 Group 8 Mini Project
+# NTU EEE EE0005 2021 Semester 1 Group 8 Mini Project
+Github Link: https://github.com/Than-Duc-Huy/Cardiovascular-Disease
 
-This is the Streamlit python program of group 8
+This is the work of EE005 Group 8 Mini Project
+Team members:
+- Than Duc Huy
+- Nguyen Hoang Minh
+- Low Thien Shawn
+- Ling Zijie
+- Tan Xin Lin
 
 
-The files that render the webpage
+# FILES
+The data files
+- Description.txt : Data description
+- cardio_train.csv : Downloaded from Kaggle (https://www.kaggle.com/sulianova/cardiovascular-disease-dataset), `;` separated
+- data.csv: Final Clean Data for Application (exported from 3_Final_Clean.ipynb)
+
+
+The Python Notebook files
+- 1_Explore.ipynb : Clean, Explore, Clustering, BMI
+- 2_Model.ipynb : Model exploration
+- 3_Final_Clean.ipynb: Final Cleaning for Application
+
+Python Streamlit Application
 - Main.py : Main page 
 
 
+# OUTLINE OF THE NOTEBOOKS
 
-The data files
-- cardio_train.csv : Downloaded from Kaggle, `;` separated
-- Description.txt : Data description
-- data.csv: Cleaned data file
-- databmi.csv: Cleaned with bmi
+## 1_Explore
+- Observer Raw data
+	- ap_hi, ap_lo outlier 
+	- convert to categorical blood pressure (bp)
+- Comment on the overall distribution of all features
+- Comment on the distribution between people with and without disease
+	- Numerical
+	- Categorical: Bayesian Probability
+- Correlation
+- Feature Engineering: BMI
+	- Distribution
+	- Obesity category
+- Clustering (KMean)
+	- On height and weight
+	- On all numerical variables
 
 
-The notebook files
-- Clean_Explore.ipynb : Clean, Explore, Export clean data
-- Clustering_BMI.ipynb : Explore Clustering and BMI 
-- Model_Exploration.ipynb : Model exploration
+## 2_Model
+- Concepts & Experiment (Using Decision Tree)
+	- Feature Scaling
+	- Train-Test-split
+	- Confusion Matrix & ROC curve
+	- GridSearchCV & Validation
 
+- Fine Tune Decision Tree
+	- Finalized the data cleaning process (3_Final_Clean)
+	- Final Data Distribution check
+	- Use the above concepts with (3_Final_Clean) data
+
+- Explore Models
+	- Use the above concepts and metrics with (3_Final_Clean) data for
+		- Random Forest Classifier
+		- Gradient Boosting Classifier
+		- AdaBoost Classifier
+
+
+
+## 3_Final_Clean
+- Clean the raw data for Application
+- Relevant Changes
+	- Drop Index
+	- Convert Age to years
+	- Calculate BMI (but does not use BMI because they are heavily related to height, weight)
+	- Filter humanly possible range:
+		- ap_hi [40,200]
+		- ap_lo [40,140]
+		- BMI [10,80]
+
+
+
+# APPLICATION
+
+## Cloud
+Streamlit Application Link: https://share.streamlit.io/than-duc-huy/cardiovascular-disease/main/Main.py
+
+
+## Local
 
 To run the streamlit library, install streamlit library
 `pip install streamlit`
 
 In the command prompt (Windows) or Bash (Linux), run the command
 `streamlit run Main.py`
+
+
